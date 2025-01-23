37e18acb07def71755842708ae8932fe31d9be18
Krzysztof Błażejewski <reexeeer@gmail.com>
2025-01-23 22:14:26 +0100

    ModbusData.cs - creation

---
+++ ModbusApp/Models/ModbusData.cs
@@ -0,0 +1,8 @@
+namespace ModbusApp.Models;
+
+public class ModbusData
+{
+    public bool[]? BoolData { get; set; }
+    public short[]? IntData { get; set; }
+    public float[]? FloatData { get; set; }
+}
