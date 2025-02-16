5868a2cccf99dbff450f82dfbb798d1ea5c5224b
Krzysztof Błażejewski <reexeeer@gmail.com>
2025-02-16 22:29:18 +0100

    class deleted

--- ModbusApp/Models/ModbusData.cs
+++
@@ -1,8 +0,0 @@
-namespace ModbusApp.Models;
-
-public class ModbusData
-{
-    public bool[]? BoolData { get; set; }
-    public short[]? IntData { get; set; }
-    public float[]? FloatData { get; set; }
-}
